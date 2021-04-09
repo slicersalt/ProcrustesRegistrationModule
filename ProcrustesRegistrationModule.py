21b10f54febb7fd53534c4d35e7c7cdc185f7b87
Ye Han <ye.han@kitware.com>
2021-04-08 20:21:16 -0400

    ENH: Compute and save transform, add UI

--- ProcrustesRegistrationModule.py
+++ ProcrustesRegistrationModule.py
@@ -1,12 +1,9 @@
-import os
-import unittest
-import vtk, qt, ctk, slicer
-# from vtkFiltersHybridPython import vtkProcrustesAlignmentFilter
-# from vtkFiltersKitPython import vtkMultiBlockDataGroupFilter
-# from vtkCommonKitPython import vtkPointSet
-# from vtkCommonKitPython import vtkMultiBlockDataSet
-from slicer.ScriptedLoadableModule import *
 import logging
+import os
+from pathlib import Path
+
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
 
 #
 # ProcrustesRegistrationModule
@@ -41,259 +38,238 @@ class ProcrustesRegistrationModuleWidget(ScriptedLoadableModuleWidget):
   """Uses ScriptedLoadableModuleWidget base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
+  def __init__(self, parent=None):
+    """
+    Called when the user opens the module the first time and the widget is initialized.
+    """
+    ScriptedLoadableModuleWidget.__init__(self, parent)
+    self.logic = None
 
   def setup(self):
     ScriptedLoadableModuleWidget.setup(self)
 
     # Instantiate and connect widgets ...
 
-    #
-    # Parameters Area
-    #
-    parametersCollapsibleButton = ctk.ctkCollapsibleButton()
-    parametersCollapsibleButton.text = "Parameters"
-    self.layout.addWidget(parametersCollapsibleButton)
+    # Load widget from .ui file (created by Qt Designer)
+    uiWidget = slicer.util.loadUI(self.resourcePath('UI/ProcrutesRegistration.ui'))
+    self.layout.addWidget(uiWidget)
+    self.ui = slicer.util.childWidgetVariables(uiWidget)
 
-    # Layout within the dummy collapsible button
-    parametersFormLayout = qt.QFormLayout(parametersCollapsibleButton)
+    self.ui.tableWidget_VTKFiles.setColumnCount(2)
+    self.ui.tableWidget_VTKFiles.setHorizontalHeaderLabels([' VTK files ', ' Set as reference '])
+    self.ui.tableWidget_VTKFiles.setColumnWidth(0, 300)
+    self.ui.tableWidget_VTKFiles.setColumnWidth(1, 200)
+    horizontalHeader = self.ui.tableWidget_VTKFiles.horizontalHeader()
+    horizontalHeader.setStretchLastSection(True)
+    self.ui.tableWidget_VTKFiles.verticalHeader().setVisible(True)
+    self.referenceButtonGroup = qt.QButtonGroup()
+    self.referenceButtonGroup.setExclusive(True)
 
-    #
-    # input volume selector
-    #
-    self.inputSelector1 = slicer.qMRMLNodeComboBox()
-    self.inputSelector1.nodeTypes = ["vtkMRMLModelNode"]
-    self.inputSelector1.selectNodeUponCreation = False
-    self.inputSelector1.addEnabled = False
-    self.inputSelector1.removeEnabled = False
-    self.inputSelector1.noneEnabled = False
-    self.inputSelector1.showHidden = False
-    self.inputSelector1.showChildNodeTypes = False
-    self.inputSelector1.setMRMLScene( slicer.mrmlScene )
-    self.inputSelector1.setToolTip( "Pick the input1 to the algorithm." )
-    parametersFormLayout.addRow("Input Model1: ", self.inputSelector1)
-
-    #
-    # output volume selector
-    #
-    self.inputSelector2 = slicer.qMRMLNodeComboBox()
-    self.inputSelector2.nodeTypes = ["vtkMRMLModelNode"]
-    self.inputSelector2.selectNodeUponCreation = False
-    self.inputSelector2.addEnabled = False
-    self.inputSelector2.removeEnabled = False
-    self.inputSelector2.noneEnabled = False
-    self.inputSelector2.showHidden = False
-    self.inputSelector2.showChildNodeTypes = False
-    self.inputSelector2.setMRMLScene( slicer.mrmlScene )
-    self.inputSelector2.setToolTip( "Pick the input2 to the algorithm." )
-    parametersFormLayout.addRow("Input Model2: ", self.inputSelector2)
-
-    #
-    # output volume selector
-    #
-    self.inputSelector3 = slicer.qMRMLNodeComboBox()
-    self.inputSelector3.nodeTypes = ["vtkMRMLModelNode"]
-    self.inputSelector3.selectNodeUponCreation = True
-    self.inputSelector3.addEnabled = False
-    self.inputSelector3.removeEnabled = False
-    self.inputSelector3.noneEnabled = False
-    self.inputSelector3.showHidden = False
-    self.inputSelector3.showChildNodeTypes = False
-    self.inputSelector3.setMRMLScene( slicer.mrmlScene )
-    self.inputSelector3.setToolTip( "Pick the input3 to the algorithm." )
-    parametersFormLayout.addRow("Input Model3: ", self.inputSelector3)
-
-    #
-    # Apply Button
-    #
-    self.applyButton = qt.QPushButton("Apply")
-    self.applyButton.toolTip = "Run the algorithm."
-    self.applyButton.enabled = False
-    parametersFormLayout.addRow(self.applyButton)
-
-    # connections
-    self.applyButton.connect('clicked(bool)', self.onApplyButton)
-    self.inputSelector1.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
-    self.inputSelector2.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
-    self.inputSelector3.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
-
-    # Add vertical spacer
-    self.layout.addStretch(1)
-
-    # Refresh Apply button state
-    self.onSelect()
+    self.logic = ProcrustesRegistrationModuleLogic()
+    self.ui.ApplyButton.connect('clicked(bool)', self.onApplyButton)
+    self.ui.LoadButton.connect('clicked(bool)', self.onLoadButton)
+    self.ui.SaveButton.connect('clicked(bool)', self.onSaveButton)
 
   def cleanup(self):
     pass
 
-  def onSelect(self):
-    self.applyButton.enabled = self.inputSelector1.currentNode() \
-                               and self.inputSelector2.currentNode()\
-                               and self.inputSelector3.currentNode()
+  def onLoadButton(self):
+    self.logic.loadData(
+      Path(self.ui.InputDirectory.directory),
+      self.ui.tableWidget_VTKFiles,
+      self.referenceButtonGroup
+    )
 
   def onApplyButton(self):
-    logic = ProcrustesRegistrationModuleLogic()
-    logic.run(self.inputSelector1.currentNode(), self.inputSelector2.currentNode(), self.inputSelector3.currentNode())
+    self.logic.run(
+      Path(self.ui.InputDirectory.directory),
+      self.ui.tableWidget_VTKFiles,
+      self.ui.ScalingCheckBox
+    )
+
+  def onSaveButton(self):
+    self.logic.saveData(
+      Path(self.ui.OutputDirectory.directory),
+           self.ui.tableWidget_VTKFiles,
+           self.ui.TransformCheckBox
+    )
 
 #
 # ProcrustesRegistrationModuleLogic
 #
 
 class ProcrustesRegistrationModuleLogic(ScriptedLoadableModuleLogic):
-
-  def hasModelData(self,modelNode):
-    """This is an example logic method that
-    returns true if the passed in volume
-    node has valid image data
-    """
-    if not modelNode:
-      logging.debug('hasModelData failed: no volume node')
-      return False
-    if modelNode.GetMesh() is None:
-      logging.debug('hasModelData failed: no poly data in model node')
-      return False
-    return True
-
-  def run(self, inputModel1, inputModel2, inputModel3):
-    """
-    Run the actual algorithm
-    """
+  def run(self, input: Path, table, scaling):
     logging.info('Processing started')
 
-    if not self.hasModelData(inputModel1):
-      return False
-    if not self.hasModelData(inputModel2):
-      return False
-    if not self.hasModelData(inputModel3):
-      return False
+    # Get reference id from widget
+    for row in range(table.rowCount):
+      if table.cellWidget(row, 1).layout().itemAt(0).widget().isChecked() == True:
+        self.referenceId = row
 
+    # Read inputs as multi-block polydata
     group = vtk.vtkMultiBlockDataGroupFilter()
-    group.AddInputDataObject(inputModel1.GetMesh())
-    group.AddInputDataObject(inputModel2.GetMesh())
-    group.AddInputDataObject(inputModel3.GetMesh())
+    filename = table.cellWidget(self.referenceId, 0).text
+    filepath = str(input) + '/' + filename
+    reader = vtk.vtkPolyDataReader()
+    reader.SetFileName(filepath)
+    reader.Update()
+    group.AddInputDataObject(reader.GetOutput())
+
+    for row in range(table.rowCount):
+      if row != self.referenceId:
+        filename = table.cellWidget(row, 0).text
+        filepath = str(input) + '/' + filename
+        reader = vtk.vtkPolyDataReader()
+        reader.SetFileName(filepath)
+        reader.Update()
+        group.AddInputDataObject(reader.GetOutput())
     group.Update()
+    self.inputPDs = group.GetOutput()
+
+    # Procrustes alignment
     procrustes = vtk.vtkProcrustesAlignmentFilter()
     procrustes.SetInputConnection(group.GetOutputPort())
-    procrustes.GetLandmarkTransform().SetModeToRigidBody()
+    if scaling.isChecked():
+      procrustes.GetLandmarkTransform().SetModeToSimilarity()
+    else:
+      procrustes.GetLandmarkTransform().SetModeToRigidBody()
     procrustes.Update()
 
-    poly1 = procrustes.GetOutput().GetBlock(0)
-    poly2 = procrustes.GetOutput().GetBlock(1)
-    poly3 = procrustes.GetOutput().GetBlock(2)
-
-    meanPoints = procrustes.GetMeanPoints()
-    nPoints = meanPoints.GetNumberOfPoints()
-    vertices = vtk.vtkCellArray()
-    vertices.Allocate(nPoints)
-    for ptId in range(nPoints):
-      vertex = vtk.vtkVertex()
-      vertex.GetPointIds().SetId(0, ptId)
-      vertices.InsertNextCell(vertex)
-    pointSets = vtk.vtkPolyData()
-    pointSets.SetPoints(meanPoints)
-    pointSets.SetVerts(vertices)
-
+    # Compute transforms and create mrml nodes
     scene = slicer.mrmlScene
+    self.outputPDs = procrustes.GetOutput()
+    self.transforms = list()
+    for row in range(table.rowCount):
+      if row < self.referenceId:
+        blockId = row + 1
+      elif row > self.referenceId:
+        blockId = row
+      else:
+        blockId = 0
+      inputPD = self.inputPDs.GetBlock(blockId)
+      outputPD = self.outputPDs.GetBlock(blockId)
 
-    outputModel1 = slicer.vtkMRMLModelNode()
-    outputModel1.SetScene(scene)
-    outputModel1.SetName(scene.GenerateUniqueName(inputModel1.GetName() + "_aligned"))
-    outputModel1.SetAndObservePolyData(poly1)
+      # Re-compute actual transform
+      landmarkTransform = vtk.vtkLandmarkTransform()
+      landmarkTransform.SetSourceLandmarks(inputPD.GetPoints())
+      landmarkTransform.SetTargetLandmarks(outputPD.GetPoints())
+      landmarkTransform.Update()
 
-    modelDisplay1 = slicer.vtkMRMLModelDisplayNode()
-    modelDisplay1.SetColor(0, 1, 1)  # cyan
-    modelDisplay1.SetBackfaceCulling(0)
-    modelDisplay1.SetScene(scene)
-    scene.AddNode(modelDisplay1)
-    outputModel1.SetAndObserveDisplayNodeID(modelDisplay1.GetID())
-    scene.AddNode(outputModel1)
+      # Add nodes to scene
+      modelNode = slicer.vtkMRMLModelNode()
+      modelNode.SetScene(scene)
+      modelNode.SetName(scene.GenerateUniqueName(table.cellWidget(row, 0).text))
+      modelNode.SetAndObservePolyData(inputPD)
 
-    outputModel2 = slicer.vtkMRMLModelNode()
-    outputModel2.SetScene(scene)
-    outputModel2.SetName(scene.GenerateUniqueName(inputModel2.GetName() + "_aligned"))
-    outputModel2.SetAndObservePolyData(poly2)
+      transformNode = slicer.vtkMRMLLinearTransformNode()
+      transformNode.SetName(modelNode.GetName() + "_transform")
+      scene.AddNode(transformNode)
+      transformNode.SetMatrixTransformToParent(landmarkTransform.GetMatrix())
+      modelNode.SetAndObserveTransformNodeID(transformNode.GetID())
+      self.transforms.append(transformNode)
 
-    modelDisplay2 = slicer.vtkMRMLModelDisplayNode()
-    modelDisplay2.SetColor(0, 1, 1)  # cyan
-    modelDisplay2.SetBackfaceCulling(0)
-    modelDisplay2.SetScene(scene)
-    scene.AddNode(modelDisplay2)
-    outputModel2.SetAndObserveDisplayNodeID(modelDisplay2.GetID())
-    scene.AddNode(outputModel2)
+      displayNode = slicer.vtkMRMLModelDisplayNode()
+      displayNode.SetColor(0, 1, 1)  # cyan
+      displayNode.SetBackfaceCulling(0)
+      displayNode.SetScene(scene)
+      scene.AddNode(displayNode)
+      modelNode.SetAndObserveDisplayNodeID(displayNode.GetID())
+      scene.AddNode(modelNode)
 
-    outputModel3 = slicer.vtkMRMLModelNode()
-    outputModel3.SetScene(scene)
-    outputModel3.SetName(scene.GenerateUniqueName(inputModel3.GetName() + "_aligned"))
-    outputModel3.SetAndObservePolyData(poly3)
+    # Show mean surface
+    meanPoints = procrustes.GetMeanPoints()
+    self.meanSurface = vtk.vtkPolyData()
+    self.meanSurface.DeepCopy(self.outputPDs.GetBlock(0))
+    self.meanSurface.SetPoints(meanPoints)
 
-    modelDisplay3 = slicer.vtkMRMLModelDisplayNode()
-    modelDisplay3.SetColor(0, 1, 1)  # cyan
-    modelDisplay3.SetBackfaceCulling(0)
-    modelDisplay3.SetScene(scene)
-    scene.AddNode(modelDisplay3)
-    outputModel3.SetAndObserveDisplayNodeID(modelDisplay3.GetID())
-    scene.AddNode(outputModel3)
+    meanModelNode = slicer.vtkMRMLModelNode()
+    meanModelNode.SetScene(scene)
+    meanModelNode.SetName(scene.GenerateUniqueName("mean_surface"))
+    meanModelNode.SetAndObservePolyData(self.meanSurface)
 
-    meanPointModel = slicer.vtkMRMLModelNode()
-    meanPointModel.SetScene(scene)
-    meanPointModel.SetName(scene.GenerateUniqueName("mean_points"))
-    meanPointModel.SetAndObservePolyData(pointSets)
-
-    meanPointDisplay = slicer.vtkMRMLModelDisplayNode()
-    meanPointDisplay.SetColor(1, 0, 0)  # cyan
-    meanPointDisplay.SetBackfaceCulling(0)
-    meanPointDisplay.SetScene(scene)
-    scene.AddNode(meanPointDisplay)
-    meanPointModel.SetAndObserveDisplayNodeID(meanPointDisplay.GetID())
-    scene.AddNode(meanPointModel)
+    meanDisplayNode = slicer.vtkMRMLModelDisplayNode()
+    meanDisplayNode.SetColor(1, 0, 0)  # red
+    meanDisplayNode.SetBackfaceCulling(0)
+    meanDisplayNode.SetScene(scene)
+    scene.AddNode(meanDisplayNode)
+    meanModelNode.SetAndObserveDisplayNodeID(meanDisplayNode.GetID())
+    scene.AddNode(meanModelNode)
 
     logging.info('Processing completed')
-
     return True
 
+  def loadData(self, input: Path, table, methodButtonGroup):
+    logging.info('Load data')
+
+    if not input.is_dir():
+      raise ValueError('data directory is not valid.')
+
+    # Fill a table with vtk filenames in input directory
+    valueList = list()
+    for file in os.listdir(input):
+        if file.endswith(".vtk"):
+            filepath = str(input) + '/' + file
+            print(filepath)
+            valueList.append(filepath)
+
+    table.setRowCount(len(valueList))
+    table.setColumnCount(2)
+    row = 0
+    for vtkFile in valueList:
+        # Column 0:
+        filename = os.path.basename(vtkFile)
+        labelVTKFile = qt.QLabel(filename)
+        labelVTKFile.setAlignment(0x84)
+        table.setCellWidget(row, 0, labelVTKFile)
+
+        # Column 1:
+        widget = qt.QWidget()
+        layout = qt.QHBoxLayout(widget)
+        checkBox = qt.QCheckBox()
+        layout.addWidget(checkBox)
+        layout.setAlignment(0x84)
+        layout.setContentsMargins(0, 0, 0, 0)
+        widget.setLayout(layout)
+        table.setCellWidget(row, 1, widget)
+        methodButtonGroup.addButton(checkBox)
+
+        row = row + 1
+
+  def saveData(self, output: Path, table, transform):
+    logging.info('Save data')
+
+    if not output.is_dir():
+      raise ValueError('output directory is not valid.')
+
+    for row in range(table.rowCount):
+      if row < self.referenceId:
+        blockId = row + 1
+      elif row > self.referenceId:
+        blockId = row
+      else:
+        blockId = 0
+
+      filepath = str(output) + '/' + os.path.splitext(table.cellWidget(row, 0).text)[0] + '_aligned.vtk'
+      writer = vtk.vtkPolyDataWriter()
+      writer.SetFileName(filepath)
+      writer.SetInputDataObject(self.outputPDs.GetBlock(blockId))
+      writer.Write()
+
+      # Save transforms
+      if transform.isChecked():
+        storage = slicer.vtkMRMLTransformStorageNode()
+        storage.SetFileName(str(output) + '/' + os.path.splitext(table.cellWidget(row, 0).text)[0] + ".h5")
+        storage.WriteData(self.transforms[row])
 
 class ProcrustesRegistrationModuleTest(ScriptedLoadableModuleTest):
-  """
-  This is the test case for your scripted module.
-  Uses ScriptedLoadableModuleTest base class, available at:
-  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
-  """
 
   def setUp(self):
-    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
-    """
-    slicer.mrmlScene.Clear(0)
+    pass
 
   def runTest(self):
-    """Run as few or as many tests as needed here.
-    """
     self.setUp()
     self.test_ProcrustesRegistrationModule1()
 
   def test_ProcrustesRegistrationModule1(self):
-    """ Ideally you should have several levels of tests.  At the lowest level
-    tests should exercise the functionality of the logic with different inputs
-    (both valid and invalid).  At higher levels your tests should emulate the
-    way the user would interact with your code and confirm that it still works
-    the way you intended.
-    One of the most important features of the tests is that it should alert other
-    developers when their changes will have an impact on the behavior of your
-    module.  For example, if a developer removes a feature that you depend on,
-    your test should break so they know that the feature is needed.
-
-    self.delayDisplay("Starting the test")
-    #
-    # first, get some data
-    #
-    import SampleData
-    SampleData.downloadFromURL(
-      nodeNames='FA',
-      fileNames='FA.nrrd',
-      uris='http://slicer.kitware.com/midas3/download?items=5767')
-    self.delayDisplay('Finished with download and loading')
-
-    volumeNode = slicer.util.getNode(pattern="FA")
-    logic = ProcrustesRegistrationModuleLogic()
-    self.assertIsNotNone( logic.hasImageData(volumeNode) )
-    self.delayDisplay('Test passed!')
-    """
-
+    pass
